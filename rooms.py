709a1aa221d085300df627269d9ea5c59d60eb30
mmdreza <m13r81a@gmail.come>
2024-01-27 11:08:11 +0330

    add methods of room class

--- rooms.py
+++ rooms.py
@@ -3,3 +3,19 @@ class room:
         self.room_id = room_id
         self.room_is_available = True
         self.room_type = room_type
+
+    
+    def display_room_status(self):
+        return self.room_is_available
+
+    def set_room_status(self, new_status):
+        if self.room_is_available:
+            self.room_is_available = False
+        else:
+          self.room_is_available = True
+
+    def schedule_room_maintenance(self, maintenance_type):
+        pass
+
+    def __str__(self):
+        return f"room id: {self.room_id}, type of room: {self.room_type}, room is avalebel:{self.room_is_available}"
