8e062f1daa4f5b1f15fe3f6d9f68c5e0d330eb0b
mmdreza <m13r81a@gmail.come>
2024-01-27 10:48:58 +0330

    add methods of person class

--- person.py
+++ person.py
@@ -6,3 +6,17 @@ class Person:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+    def update_contact_details(self, new_first_name=None, new_last_name=None, new_age=None):
+        self.first_name = new_first_name or self.first_name
+        self.last_name = new_last_name or self.last_name
+        self.age = int(new_age or self.age)
+
+    def __str__(self):
+        return f"First Name is: {self.first_name}, Last Name is: {self.last_name}, Age is: {self.age} !"
+    
+    @abstractmethod
+    def role(self):
+        pass
+        
+if __name__ == "__main__":
+    p1 = Person("s400" , "mmd" , "reza" , 21)
