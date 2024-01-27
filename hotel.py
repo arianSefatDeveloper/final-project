4a0205e159b099aaff3d75f61bfc9343c20688fc
mmdreza <m13r81a@gmail.come>
2024-01-27 11:09:57 +0330

    add methods of hotel class

--- hotel.py
+++ hotel.py
@@ -7,3 +7,16 @@ from admin import *
 class Hotel:
     def __init__(self, name):
         self.name = name
+
+    def list_available_rooms(self, room_type):
+        pass
+
+    def get_guest_details(self, guest_id):
+        pass
+
+    def summerize_daily_operations(self):
+        pass
+
+
+if __name__ == "__main__":
+    p1 = Hotel("shiraz")
