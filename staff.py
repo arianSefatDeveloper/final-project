4bbbcce948deb1d62397b1b138d66b5cd09ec251
mmdreza <m13r81a@gmail.come>
2024-01-28 06:43:58 +0330

    book_guest method

--- staff.py
+++ staff.py
@@ -1,13 +1,22 @@
 from person import *
-
+from room import *
+from guest import *
+from admin import *
+from hotel import *
 class Staff(Person):
     def __init__(self, id_, first_name, last_name, age, role):
         super().__init__(id_, first_name, last_name, age)
         self.role = role
+        self.guest_room = {}
     
       # receptionist
-    def book_guest(self, guest_id):
-        pass
+    def book_guest(self,  room ,room_id,  guest_id):
+        if guest_id not in self.guest_room and room.room_is_available:
+            self.guest_room[guest_id] = room_id
+            room.room_is_available = False
+            print(" booking successfully!")
+        else:
+            print("exists")
     def check_out_guest(self, guest_id):
         pass
 
@@ -23,7 +32,6 @@ class Staff(Person):
         pass
 
     def order_repair_materials(self, material_list):
-        pass
-
+        pass  
 if __name__ == "__main__":
     s1 = Staff("s400" , "mmd" , "reza" , 21)
